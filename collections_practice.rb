34f3fedd119054f0dd7466eb870fda863f2de88a
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-11-06 14:50:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(array)
+ array.sort_by{ |number| number}
+ end 
+ 
+def sort_array_desc(array)
+ array.sort_by{ |number| -number}
+ end 
+ 
+def sort_array_char_count(array)
+  array.sort_by{ |number| number.length}
+end 
 
+def swap_elements(array)
+  array = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker
+  
+end  
